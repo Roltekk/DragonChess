5680ae656f69448e97a72f4d7be7d4c155f53ddd
Rolzad73 <rolzad73@gmail.com>
2013-08-27 11:22:55 -0400

    - fixed Android and Desktop projects build issue - updated .gitignore files

--- game/dragonchess-android/src/com/roltekk/game/dragonchess_android/DragonChessAndroid.java
+++ game/dragonchess-android/src/com/roltekk/game/dragonchess_android/DragonChessAndroid.java
@@ -1,10 +1,11 @@
 package com.roltekk.game.dragonchess_android;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.roltekk.game.dragonchess_core.DragonChessCore;
 
 public class DragonChessAndroid extends AndroidApplication {
-        public void onCreate (android.os.Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                initialize(new Game(), false);
-        }
+  public void onCreate(android.os.Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    initialize(new DragonChessCore(), false);
+  }
 }
