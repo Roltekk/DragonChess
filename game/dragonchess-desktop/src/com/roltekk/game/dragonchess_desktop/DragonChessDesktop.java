9ad8665bf1ab5ae36032eaac2a03ebd2abb3c7e0
Rolzad73 <rolzad73@gmail.com>
2013-09-08 04:21:00 -0400

    - purposely testing broken build to test Jenkins email notification mechanism.

--- game/dragonchess-desktop/src/com/roltekk/game/dragonchess_desktop/DragonChessDesktop.java
+++ game/dragonchess-desktop/src/com/roltekk/game/dragonchess_desktop/DragonChessDesktop.java
@@ -5,6 +5,6 @@ import com.roltekk.game.dragonchess_core.DragonChessCore;
 
 public class DragonChessDesktop {
   public static void main(String[] args) {
-    new LwjglApplication(new DragonChessCore(), "Game", 480, 320, false);
+    new iLwjglApplication(new DragonChessCore(), "Game", 480, 320, false);
   }
 }
