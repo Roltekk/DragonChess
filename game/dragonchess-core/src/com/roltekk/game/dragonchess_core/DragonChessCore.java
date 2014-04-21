7aef04b92d7eda85cb890cb5fb948cd6064e78b4
Rolzad73 <rolzad73@gmail.com>
2014-04-21 10:33:20 -0400

    Test Jenkins build

--- game/dragonchess-core/src/com/roltekk/game/dragonchess_core/DragonChessCore.java
+++ game/dragonchess-core/src/com/roltekk/game/dragonchess_core/DragonChessCore.java
@@ -19,7 +19,7 @@ import com.roltekk.game.dragonchess_core.screens.TitleScreen;
 
 public class DragonChessCore extends Game {
   private static final String TAG = "DragonChessCore";
-  private Music               mBGMusic;t
+  private Music               mBGMusic;
 
   // ApplicationListener overrides ////////////////////////////////////////////
   @Override
